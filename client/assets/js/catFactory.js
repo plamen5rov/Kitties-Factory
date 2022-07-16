a674d070ef43de92f5bfcedf1871d541956be599
Plamen <biznetmen@gmail.com>
2022-07-16 12:59:45 +0300

    animations slider aded

--- client/assets/js/catFactory.js
+++ client/assets/js/catFactory.js
@@ -5,49 +5,49 @@ function getColor() {
     return randomColor
 }
 
-function genColors(){
+function genColors() {
     var colors = []
-    for(var i = 10; i < 99; i ++){
-      var color = getColor()
-      colors[i] = color
+    for (var i = 10; i < 99; i++) {
+        var color = getColor()
+        colors[i] = color
     }
     return colors
 }
 
 //This function code needs to modified so that it works with Your cat code.
-function headColor(color,code) {
+function headColor(color, code) {
     $('.cat__head, .cat__chest').css('background', '#' + color)  //This changes the color of the cat
-    $('#headcode').html('code: '+code) //This updates text of the badge next to the slider
+    $('#headcode').html('code: ' + code) //This updates text of the badge next to the slider
     $('#dnabody').html(code) //This updates the body color part of the DNA that is displayed below the cat
 }
 
-function mouthColor(color,code) {
+function mouthColor(color, code) {
     $('.cat__mouth-contour, .cat__chest_inner, .cat__tail').css('background', '#' + color)  //This changes the color of the cat
-    $('#mouthcode').html('code: '+code) //This updates text of the badge next to the slider
+    $('#mouthcode').html('code: ' + code) //This updates text of the badge next to the slider
     $('#dnamouth').html(code) //This updates the body color part of the DNA that is displayed below the cat
 }
 
-function eyesColor(color,code) {
+function eyesColor(color, code) {
     $('.cat__eye span').css('background', '#' + color)  //This changes the color of the cat
-    $('#eyescode').html('code: '+code) //This updates text of the badge next to the slider
+    $('#eyescode').html('code: ' + code) //This updates text of the badge next to the slider
     $('#dnaeyes').html(code) //This updates the body color part of the DNA that is displayed below the cat
 }
 
-function earsColor(color,code) {
+function earsColor(color, code) {
     $('.cat__ear--left, .cat__ear--right, .cat__paw-left, .cat__paw-right, .cat__paw-left_inner, .cat__paw-right_inner').css('background', '#' + color)  //This changes the color of the cat
-    $('#earscode').html('code: '+code) //This updates text of the badge next to the slider
+    $('#earscode').html('code: ' + code) //This updates text of the badge next to the slider
     $('#dnaears').html(code) //This updates the body color part of the DNA that is displayed below the cat
 }
 
-function decorationMidcolor(color,code) {
+function decorationMidcolor(color, code) {
     $('.cat__head-dots').css('background', '#' + color)  //This changes the color of the cat
-    $('#decorationMidCode').html('code: '+code) //This updates text of the badge next to the slider
+    $('#decorationMidCode').html('code: ' + code) //This updates text of the badge next to the slider
     $('#dnadecorationMid').html(code) //This updates the body color part of the DNA that is displayed below the cat
 }
 
-function decorationSidescolor(color,code) {
+function decorationSidescolor(color, code) {
     $('.cat__head-dots_first, .cat__head-dots_second').css('background', '#' + color)  //This changes the color of the cat
-    $('#decorationSidesCode').html('code: '+code) //This updates text of the badge next to the slider
+    $('#decorationSidesCode').html('code: ' + code) //This updates text of the badge next to the slider
     $('#dnadecorationSides').html(code) //This updates the body color part of the DNA that is displayed below the cat
 }
 //###################################################
@@ -162,29 +162,72 @@ function normaldecoration() {
 }
 
 function decoration2() {
-    
+
     $('.cat__head-dots').css({ "transform": "rotate(15deg)", "height": "48px", "width": "14px", "top": "1px", "border-radius": "0 0 50% 50%" })
     $('.cat__head-dots_first').css({ "transform": "rotate(15deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "50% 0 50% 50%" })
     $('.cat__head-dots_second').css({ "transform": "rotate(15deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "0 50% 50% 50%" })
 }
 
 function decoration3() {
-    
+
     $('.cat__head-dots').css({ "transform": "rotate(20deg)", "height": "48px", "width": "14px", "top": "1px", "border-radius": "0 0 50% 50%" })
     $('.cat__head-dots_first').css({ "transform": "rotate(20deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "50% 0 50% 50%" })
     $('.cat__head-dots_second').css({ "transform": "rotate(20deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "0 50% 50% 50%" })
 }
 
 function decoration4() {
-    
+
     $('.cat__head-dots').css({ "transform": "rotate(25deg)", "height": "48px", "width": "14px", "top": "1px", "border-radius": "0 0 50% 50%" })
     $('.cat__head-dots_first').css({ "transform": "rotate(25deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "50% 0 50% 50%" })
     $('.cat__head-dots_second').css({ "transform": "rotate(25deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "0 50% 50% 50%" })
 }
 
 function decoration5() {
-    
+
     $('.cat__head-dots').css({ "transform": "rotate(30deg)", "height": "48px", "width": "14px", "top": "1px", "border-radius": "0 0 50% 50%" })
     $('.cat__head-dots_first').css({ "transform": "rotate(30deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "50% 0 50% 50%" })
     $('.cat__head-dots_second').css({ "transform": "rotate(30deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "0 50% 50% 50%" })
+}
+
+function animationVariation(num) {
+    $('#dnaanimation').html(num)
+    switch (num) {
+        case 1:
+            animation1()
+            break
+        case 2:
+            animation2()
+            break
+        case 3:
+            animation3()
+            break
+        case 4:
+            animation4()
+            break
+    }
+}
+
+function animation1() {
+    $('#animationCode').html('None')
+
+}
+
+function animation2() {
+    $('#head').addClass('movingHead')
+    $('#animationCode').html('Head')
+
+}
+
+function animation3() {
+    $('#head').removeClass('movingHead')
+    $('.cat__ear').addClass('movingHead')
+    $('#animationCode').html('Ears')
+
+}
+
+function animation4() {
+    $('#head').addClass('movingHead')
+    $('.cat__ear').addClass('movingHead')
+    $('#animationCode').html('Head & ears')
+
 }
--- client/assets/js/catSettings.js
+++ client/assets/js/catSettings.js
@@ -2,26 +2,26 @@
 var colors = Object.values(allColors())
 
 var defaultDNA = {
-    "headcolor" : 10,
-    "mouthColor" : 13,
-    "eyesColor" : 96,
-    "earsColor" : 10,
-    //Cattributes
-    "eyesShape" : 1,
-    "decorationPattern" : 1,
-    "decorationMidcolor" : 13,
-    "decorationSidescolor" : 13,
-    "animation" :  1,
-    "lastNum" :  1
-    }
+  "headcolor": 10,
+  "mouthColor": 13,
+  "eyesColor": 96,
+  "earsColor": 10,
+  //Cattributes
+  "eyesShape": 1,
+  "decorationPattern": 1,
+  "decorationMidcolor": 13,
+  "decorationSidescolor": 13,
+  "animation": 1,
+  "lastNum": 1
+}
 
 // when page load
-$( document ).ready(function() {
+$(document).ready(function () {
   $('#dnabody').html(defaultDNA.headColor);
   $('#dnamouth').html(defaultDNA.mouthColor);
   $('#dnaeyes').html(defaultDNA.eyesColor);
   $('#dnaears').html(defaultDNA.earsColor);
-    
+
   $('#dnashape').html(defaultDNA.eyesShape)
   $('#dnadecoration').html(defaultDNA.decorationPattern)
   $('#dnadecorationMid').html(defaultDNA.decorationMidcolor)
@@ -32,81 +32,90 @@ $( document ).ready(function() {
   renderCat(defaultDNA)
 });
 
-function getDna(){
-    var dna = ''
-    dna += $('#dnabody').html()
-    dna += $('#dnamouth').html()
-    dna += $('#dnaeyes').html()
-    dna += $('#dnaears').html()
-    dna += $('#dnashape').html()
-    dna += $('#dnadecoration').html()
-    dna += $('#dnadecorationMid').html()
-    dna += $('#dnadecorationSides').html()
-    dna += $('#dnaanimation').html()
-    dna += $('#dnaspecial').html()
+function getDna() {
+  var dna = ''
+  dna += $('#dnabody').html()
+  dna += $('#dnamouth').html()
+  dna += $('#dnaeyes').html()
+  dna += $('#dnaears').html()
+  dna += $('#dnashape').html()
+  dna += $('#dnadecoration').html()
+  dna += $('#dnadecorationMid').html()
+  dna += $('#dnadecorationSides').html()
+  dna += $('#dnaanimation').html()
+  dna += $('#dnaspecial').html()
 
-    return parseInt(dna)
+  return parseInt(dna)
 }
 
-function renderCat(dna){
-    headColor(colors[dna.headcolor],dna.headcolor);
-    $('#bodycolor').val(dna.headcolor);
-    mouthColor(colors[dna.mouthColor],dna.mouthColor);
-    $('#mouthcolor').val(dna.mouthColor);
-    eyesColor(colors[dna.eyesColor],dna.eyesColor);
-    $('#eyescolor').val(dna.eyesColor);
-    earsColor(colors[dna.earsColor],dna.earsColor);
-    $('#earscolor').val(dna.earsColor);
-    decorationMidcolor(colors[dna.decorationMidcolor],dna.decorationMidcolor);
-    $('#decorationMid').val(dna.decorationMidcolor);
-    decorationSidescolor(colors[dna.decorationSidescolor],dna.decorationSidescolor);
-    $('#decorationSides').val(dna.decorationSidescolor);
+function renderCat(dna) {
+  headColor(colors[dna.headcolor], dna.headcolor);
+  $('#bodycolor').val(dna.headcolor);
+  mouthColor(colors[dna.mouthColor], dna.mouthColor);
+  $('#mouthcolor').val(dna.mouthColor);
+  eyesColor(colors[dna.eyesColor], dna.eyesColor);
+  $('#eyescolor').val(dna.eyesColor);
+  earsColor(colors[dna.earsColor], dna.earsColor);
+  $('#earscolor').val(dna.earsColor);
+  decorationMidcolor(colors[dna.decorationMidcolor], dna.decorationMidcolor);
+  $('#decorationMid').val(dna.decorationMidcolor);
+  decorationSidescolor(colors[dna.decorationSidescolor], dna.decorationSidescolor);
+  $('#decorationSides').val(dna.decorationSidescolor);
+  animationVariation(dna.animation);
+  $('#animation').val(dna.animation);
+
 }
 
 // Changing cat colors
-$('#bodycolor').change(()=>{
+$('#bodycolor').change(() => {
   var colorVal = $('#bodycolor').val()
-  headColor(colors[colorVal],colorVal)
+  headColor(colors[colorVal], colorVal)
 })
 
 // Changing mouth colors
-$('#mouthcolor').change(()=>{
+$('#mouthcolor').change(() => {
   var colorVal = $('#mouthcolor').val()
-  mouthColor(colors[colorVal],colorVal)
+  mouthColor(colors[colorVal], colorVal)
 })
 
 // Changing eyes colors
-$('#eyescolor').change(()=>{
+$('#eyescolor').change(() => {
   var colorVal = $('#eyescolor').val()
-  eyesColor(colors[colorVal],colorVal)
+  eyesColor(colors[colorVal], colorVal)
 })
 
 // Changing ears colors
-$('#earscolor').change(()=>{
+$('#earscolor').change(() => {
   var colorVal = $('#earscolor').val()
-  earsColor(colors[colorVal],colorVal)
+  earsColor(colors[colorVal], colorVal)
 })
 
 // Changing eye shape
-$('#eyeshape').change(()=>{
-  var shape = parseInt($('#eyeshape').val()) 
+$('#eyeshape').change(() => {
+  var shape = parseInt($('#eyeshape').val())
   eyeVariation(shape)
 })
 
 // Changing decoration shape
-$('#decorationPattern').change(()=>{
-  var shape = parseInt($('#decorationPattern').val()) 
+$('#decorationPattern').change(() => {
+  var shape = parseInt($('#decorationPattern').val())
   decorationVariation(shape)
 })
 
 // Changing Mid Hair colors
-$('#decorationMid').change(()=>{
+$('#decorationMid').change(() => {
   var colorVal = $('#decorationMid').val()
-  decorationMidcolor(colors[colorVal],colorVal)
+  decorationMidcolor(colors[colorVal], colorVal)
 })
 
 // Changing Side Hair colors
-$('#decorationSides').change(()=>{
+$('#decorationSides').change(() => {
   var colorVal = $('#decorationSides').val()
-  decorationSidescolor(colors[colorVal],colorVal)
+  decorationSidescolor(colors[colorVal], colorVal)
+})
+
+// Animations
+$('#animation').change(() => {
+  var animationVal = parseInt($('#animation').val())
+  animationVariation(animationVal)
 })
