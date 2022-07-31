8ae548c10143645dcfafaa66a1ba4565023a98ae
Plamen <biznetmen@gmail.com>
2022-07-31 09:07:01 +0300

    birth event notification added

--- client/index.js
+++ client/index.js
@@ -11,17 +11,37 @@ $(document).ready(function () {
 
         //console.log(instance);
 
-        function createKitty() {
-            var dnaStr = getDna();
-            instance.methods.createKittyGen0(dnaStr).send({}, function (err, txHash) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log(txHash);
-                }
-            });
+        instance.events.Birth().on('data', function (event) {
+            console.log(event);
+            let owner = event.returnValues.owner;
+            let kittenID = event.returnValues.kittenID;
+            let mumID = event.returnValues.mumID;
+            let dadID = event.returnValues.dadID;
+            let genes = event.returnValues.genes;
+            $("#kittyCreation").css("display", "block");
+            $("#kittyCreation").text("Owner: " + owner
+                + " Kitty ID: " + kittenID
+                + " Mum ID: " + mumID
+                + " Dad ID: " + dadID
+                + " Genes: " + genes)
+        }).on('error', console.error);
+    })
+})
+
+
+function createKitty() {
+    var dnaStr = getDna();
+    instance.methods.createKittyGen0(dnaStr).send({}, function (err, txHash) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(txHash);
         }
-
-
     });
-});
+}
+
+
+
+$('#createCatButton').click(() => {
+    createKitty(getDna());
+})
